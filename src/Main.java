4cb6bc812a407ef9a7ec7af66492dc2c099d33c2
Maksim Patrakeev <patrakeev119@gmail.com>
2021-04-03 00:15:58 +0400

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int prise = 2000000000;
+        int count = 500000000;
+        int totalVolume = prise + count;
+        System.out.println("totalVolume");
+    }
 }
